b8b72a5c95f957fafb44b2b6b396ce1b7f4d254b
sevac <sevac@VS_KUZNEC>
2023-06-14 12:53:56 +0300

    Добавьте файлы проекта.

---
+++ Project 8.6.2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+
+namespace Task2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                DirectoryInfo Folder = new DirectoryInfo("C:\\Repository");
+                long TotalFolderSize = FolderSize(Folder);
+                Console.WriteLine("Размер папки в байтах: " + TotalFolderSize);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        static long FolderSize(DirectoryInfo Folder)
+        {
+            if (Folder.Exists)
+            {
+                long TotalSizeOfDir = 0;
+
+                FileInfo[] AllFiles = Folder.GetFiles();
+                foreach (FileInfo File in AllFiles)
+                {
+                    TotalSizeOfDir += File.Length;
+                }
+
+                DirectoryInfo[] SubFolders = Folder.GetDirectories();
+                foreach (DirectoryInfo dir in SubFolders)
+                {
+                    TotalSizeOfDir += FolderSize(dir);
+                }
+
+                return TotalSizeOfDir;
+
+            }
+            else
+            {
+                Console.WriteLine("Такой папки не существует");
+                return 0;
+            }
+        }
+    }
+}
